950e677f86264c978951ede95626fc77460cb7db
Monir <hossinmonir681@gmail.com>
2023-11-22 08:19:11 +0600

    project one setup

--- milestone-2/4-first-project/src/app.ts
+++ milestone-2/4-first-project/src/app.ts
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//aprsers
+app.use(express.json());
+app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+
+  res.send(a);
+});
+
+export default app;
--- milestone-2/4-first-project/src/server.ts
+++ milestone-2/4-first-project/src/server.ts
@@ -1,15 +1,19 @@
-const mongoose = require('mongoose');
-
-
-
+import mongoose from 'mongoose';
+import config from './app/config';
+import app from './app';
 
 async function main() {
-    await mongoose.connect(process.env.DATABASE_URL);
-  
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
   }
 
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+main();
